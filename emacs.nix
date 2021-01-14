5f8e5b66e6cc170ffba0f2cd18f8df8f01998805
Giorgi Lekveishvili <giolekva@gmail.com>
2021-01-14 21:21:51 +0400

    emacs: key bindings

--- emacs.nix
+++ emacs.nix
@@ -6,22 +6,37 @@
       recommendedGcSettings = true;
       startupTimer = true;
       postlude = ''
-        (global-set-key [(meta l)] 'goto-line)
-        (global-set-key [(meta c)] 'comment-region)
-        (global-set-key [(meta u)] 'uncomment-region)
-        (setq 'confirm-kill-emacs (quote y-or-n-p))
-        (setq inhibit-startup-message t)
-        (setq initial-scratch-message nil)
-        (defalias 'yes-or-no-p 'y-or-n-p)
+        ;; Key bindings
+        (global-set-key [(ctrl l)] 'goto-line)
+        (global-set-key [(ctrl c) (c)] 'comment-region)
+        (global-set-key [(ctrl c) (u)] 'uncomment-region)
+        (global-set-key [(ctrl c) (r)] 'query-replace)
+
+        ;; Hide UI
         (tool-bar-mode -1)
         (scroll-bar-mode -1)
         (menu-bar-mode -1)
+
+        ;; Line/Column number mode
         (line-number-mode)
         (column-number-mode)
-        (setq-default show-trailing-whitespace t)
-        (setq make-backup-files nil)
+
+        ;; Parenthesis
         (setq show-paren-mode t)
         (setq show-paren-delay 0)
+
+        ;; Yes/No
+        (setq 'confirm-kill-emacs (quote y-or-n-p))
+        (defalias 'yes-or-no-p 'y-or-n-p)
+
+        ;; Startup
+        (setq inhibit-startup-message t)
+        (setq initial-scratch-message nil)
+
+        ;; Disable backup files
+        (setq make-backup-files nil)
+
+        (setq-default show-trailing-whitespace t)
         (prefer-coding-system 'utf-8)
       '';
       usePackage = {
