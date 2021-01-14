99a118765a912eedba523c76d8aef5084c0054f8
Giorgi Lekveishvili <giolekva@gmail.com>
2021-01-14 21:21:28 +0400

    bash: git subcommand aliases

--- bash.nix
+++ bash.nix
@@ -9,8 +9,14 @@
       ".." = "cd ..";
       bz = "bazelisk";
       bazel = "bazelisk";
+      e = "emacs -nw";
       enw = "emacs -nw";
       g = "git";
+      ga = "git add";
+      gc = "git commit -m";
+      gd = "git diff";
+      gp = "git push";
+      gs = "git status";
       hm = "home-manager";
       k = "kubectl";
       ll = "ls -l";
